11faa9c3b55306bb636e289ae09548a0ae105f43
Bashbuilder <iamadelowotan@gmail.com>
2026-07-13 14:14:34 +0100

    feat: added a publishing rapper for docker upload

--- api/obj/Release/net10.0/api.AssemblyInfo.cs
+++ api/obj/Release/net10.0/api.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c87587319a022fa977f5dae651869ce67f88d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
